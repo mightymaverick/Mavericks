1f087c8e0624a52ba14ff43fa46a5cc9ea1d7b26
Senthil Kumar Mohan <senthilkumarmohan@hotmail.com>
2016-06-09 20:22:28 +0530

    Added additional entities.

---
+++ MightyMaveryk/MavericksEntity/EventsEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aazhi.MightyMaverick.MavericksEntity
+{
+    public class EventsEntity
+    {
+        public Nullable<int> EventId { get; set; }
+
+        public Nullable<int> UserId { get; set; }
+
+        /// <summary>
+        /// Birtyday, Anniversary, Death Anniversary, etc
+        /// </summary>
+        public int EventType { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        /// <summary>
+        /// For setting whether the event is Private or Public
+        /// </summary>
+        public int EventAccess { get; set; }
+
+        public bool IsReminderSet { get; set; }
+
+        /// <summary>
+        /// Phone, Greeting Card, E-Mail, etc
+        /// </summary>
+        public int ReminderType { get; set; }
+
+        public DateTime ReminderTime { get; set; }
+                
+        public string EventCreatedBy { get; set; }
+
+        public DateTime EventCreatedDate { get; set; }
+
+        public string EventModifiedBy { get; set; }
+
+        public Nullable<DateTime> EventModifiedDate { get; set; }
+    }
+}
---
+++ MightyMaveryk/MavericksEntity/MediaEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aazhi.MightyMaverick.MavericksEntity
+{
+    public class MediaEntity
+    {
+        public Nullable<int> MediaId { get; set; }
+
+        public Nullable<int> UserId { get; set; }
+
+        public Nullable<int> EventId { get; set; }
+
+        /// <summary>
+        /// Photo or Video
+        /// </summary>
+        public int MediaType { get; set; }
+
+        /// <summary>
+        /// Profile Picture, Album Image, Thumbnail, etc
+        /// </summary>
+        public int MediaUsageType { get; set; }
+
+
+    }
+}
